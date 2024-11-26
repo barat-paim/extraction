dd937abb63d6c83d700e87ff1527a9cb60042e7d
barath <barat.paim@gmail.com>
2024-11-26 17:31:44 -0500

    V1.1

--- mercury/pull.py
+++ mercury/pull.py
@@ -1,8 +1,7 @@
 import requests
 import os
 
-# Test the API
-url = f"https://bustime.mta.info/api/siri/vehicle-monitoring.json"
+url = "https://bustime.mta.info/api/siri/vehicle-monitoring.json"
 params = {
     "key": os.getenv('MTA_API_KEY'),
     'LineRef': 'M57'
@@ -10,8 +9,27 @@ params = {
 
 try:
     response = requests.get(url, params=params)
-    print("API Response Status: ", response.status_code)
-    print("API Response Data: ", response.json())
+    data = response.json()
+    
+    # Get vehicle activities
+    vehicles = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+    
+    print(f"\nFound {len(vehicles)} M57 buses:")
+    for bus in vehicles:
+        journey = bus['MonitoredVehicleJourney']
+        location = journey['VehicleLocation']
+        direction = "Eastbound" if journey['DirectionRef'] == '0' else "Westbound"
+        
+        print(f"\nBus {journey['VehicleRef']}:")
+        print(f"Direction: {direction}")
+        print(f"Location: {location['Latitude']}, {location['Longitude']}")
+        
+        if 'MonitoredCall' in journey:
+            stop = journey['MonitoredCall']
+            print(f"Next Stop: {stop['StopPointName']}")
+            if 'EstimatedPassengerCount' in stop['Extensions']['Capacities']:
+                print(f"Passengers: {stop['Extensions']['Capacities']['EstimatedPassengerCount']}/80")
+
 except Exception as e:
     print("Error: ", e)
 
